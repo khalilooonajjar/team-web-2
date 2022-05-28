3de906977714c85c728e8831656527a1c3448e54
Khalil Najjar <92646950+khalilooonajjar@users.noreply.github.com>
2022-05-28 21:43:40 +0300

    Add files via upload

---
+++ reactjs-login-register-crud/src/pages/index.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import Header from "../elements/header";
+import Sidebar from "../elements/sidebar";
+import {Link, Redirect} from 'react-router-dom';
+import axios from 'axios';
+
+export default class Index extends Component {
+    state = {
+        employees: [],
+        toDashboard: false,
+        isLoading: false
+    };
+
+    constructor(props) {
+        super(props);
+        this.url = 'https://gowtham-rest-api-crud.herokuapp.com/employees';
+        this.token = localStorage.getItem('token');
+    }
+
+    componentDidMount() {
+        axios.get(this.url , { params: { token: this.token}})
+            .then(response => {
+                const employees = response.data.data.employees;
+                this.setState({ employees });
+            })
+            .catch(error => {
+                this.setState({ toDashboard: true });
+                console.log(error);
+            });
+    }
+
+    handleClickDelete = event => {
+        axios.delete(this.url + '/' + event.target.value , { params: { token: this.token}})
+            .then(response => {
+                this.componentDidMount();
+                this.setState({ isLoading: true})
+            })
+            .catch( error => {
+                console.log(error.toString());
+                this.setState({ toDashboard: true });
+            });
+    };
+
+    render() {
+        if (this.state.toDashboard === true) {
+            return <Redirect to='/' />
+        }
+        return (
+            <div>
+                <Header/>
+                <div id="wrapper">
+                    <Sidebar/>
+                    <div id="content-wrapper">
+                        <div className="container-fluid">
+                            <ol className="breadcrumb">
+                                <li className="breadcrumb-item">
+                                    <Link to={'/dashboard'} >Dashboard</Link>
+                                </li>
+                                <li className="breadcrumb-item active">CRUD App</li>
+                                <li className="ml-auto"><Link to={'add'}>Add Employee</Link></li>
+                            </ol>
+                            <div className="card mb-3">
+                                <div className="card-header"><i className="fas fa-table"></i>
+                                    &nbsp;&nbsp;Employees List
+                                </div>
+                                <div className="card-body">
+                                    <table className="table table-bordered">
+                                        <thead>
+                                        <tr>
+                                           
+                                            <th className="text-center">Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                            {this.state.employees.map((employees , index)=>
+                                                <tr key={employees.id}>
+                                                    <td>{index + 1}</td>
+                                                    <td>{employees.name}</td>
+                                              
+                                                    <td className="text-center">
+                                                        <Link className="btn btn-sm btn-info" to={{ pathname: 'edit', search: '?id=' + employees.id }}>Edit</Link>
+                                                        &nbsp; | &nbsp;
+                                                        <button value={employees.id} className="btn btn-sm btn-danger" disabled={ index === 0  ? true : false} onClick={this.handleClickDelete} >Delete</button>
+                                                    </td>
+                                                </tr>)
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <footer className="sticky-footer">
+                            <div className="container my-auto">
+                                <div className="copyright text-center my-auto">
+                                </div>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
